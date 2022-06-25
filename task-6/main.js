b38155d39920e61590e29a1f8608b1f5c6a91dc1
nesrinmirza <106273121+nesrinmirza@users.noreply.github.com>
2022-06-25 04:20:08 -0700

    Update main.js

--- task-6/main.js
+++ task-6/main.js
@@ -1,9 +1,19 @@
-const prompt = require('prompt');
-prompt.start();
-
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+let max = 0;
+let min = arr[0];
+let a = 0;
+for (let i = 0; i < arr.length; i++) {
+    a = arr.length-1;
+    if (arr[i]>max) {
+        max = arr[i];
+    }
+    else if (arr[i]<min) {
+        min = arr[i];
+    }
+    
+    }
+    if (a>=2) {
+        console.log(min, max);
+     }
+     else if (a<2) {
+        console.log("Ooops");
+     }
